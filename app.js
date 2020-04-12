e27b58fb83af776ff3c0c667b5936321deb4268d
DanielVega-Smll94 <31718956+DanielVega-Smll94@users.noreply.github.com>
2020-04-11 19:06:14 -0500

    primer cambio

---
+++ app.js
@@ -0,0 +1,14 @@
+//document.getElementById('app').innerHTML= 'Hola mundo'
+//let nombre = prompt('Cual es tu nombre');
+//let edad = prompt('Cual es tu edad');
+//document.getElementById('app').innerHTML= `Bienvenido ${nombre} de ${edad} Años`; 
+var dias = 99;
+console.log(dias);
+var primerNombre= 'Juan';
+var primer_nombre = 'marto';
+let producto = 'Libro';
+producto  = 'Libro 1';
+console.log(producto);
+
+const prod = 'libra';
+console.log(prod);
